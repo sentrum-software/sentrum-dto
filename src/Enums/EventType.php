fc93c109383b4f76e49f152a120cf260a2649a92
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-04-24 15:48:40 +0300

    Added library updated event

--- src/Enums/EventType.php
+++ src/Enums/EventType.php
@@ -8,4 +8,6 @@ enum EventType: string
     case UPDATE_ORDER = 'update_order';
     case NEW_SHIPMENT = 'new_shipment';
     case UPDATE_SHIPMENT = 'update_shipment';
+    case NEW_LIBRARY = 'new_library';
+    case UPDATE_LIBRARY = 'update_library';
 }
